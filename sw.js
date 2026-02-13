9932834e702d3c1792c4cc2a198f1e234fd59a7e
raffyexconde21-png <raffyexconde21@gmail.com>
2026-02-13 08:44:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Very simple offline cache for PWA
+const CACHE = 'exin-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  const req=e.request;
+  e.respondWith(
+    caches.match(req).then(cached=> cached || fetch(req).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
